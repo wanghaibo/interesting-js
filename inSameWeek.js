e8c2f4503ad92121dde6bd2899783e82891512b9
wanghaibo@myhexin.com <wanghaibo@myhexin.com>
2015-06-12 14:24:22 +0800

    add in same week

---
+++ inSameWeek.js
@@ -0,0 +1,14 @@
+var inSameWeek = (function() {
+    var strToDate = function(dateStr) {
+        dateStr += ''; 
+        return new Date(dateStr.substr(0, 4), (dateStr.substr(4, 2)-1), dateStr.substr(6, 2));
+    };
+    return function() {
+        var dateStrs = Array.prototype.slice.call(arguments),
+        minDate = strToDate(Math.min.apply(Math, dateStrs)),
+        maxDate = strToDate(Math.max.apply(Math, dateStrs));
+        return (minDate.getDay()+(maxDate.getTime()-minDate.getTime())/86400000) <= 6;
+    }
+})()
+
+inSameWeek("20141214", '20141216', '20141218', 20141220);
